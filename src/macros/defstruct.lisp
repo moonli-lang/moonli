8f12692ae7099f7a78e0aa802f9d5781f1db3ce5
Shubhamkar Ayare <digikar@proton.me>
2025-12-17 06:48:02 +0100

    [add] allow defstruct slot default value without parens

--- src/macros/defstruct.lisp
+++ src/macros/defstruct.lisp
@@ -23,49 +23,67 @@
                                 "="
                                 +whitespace/internal
                                 chain)))
+             (and expr:symbol
+                  +whitespace/internal
+                  "="
+                  +whitespace/internal
+                  moonli-expression)
              (and expr:symbol))
          #\;
          *whitespace)
   (:function (lambda (expr)
-               (destructuring-bind
-                   (first &optional second third &rest ign) (first expr)
-                 (declare (ignore ign))
-                 (if (symbolp first)
-                     first
-                     (nconc (list (nth 2 first)
-                                  (nth 6 first))
-                            (unless (null second)
-                              (list :type (nth 3 second)))
-                            (unless (null third)
-                              (list (nth 2 third)
-                                    (nth 6 third)))))))))
+               (cond ((= 5 (length (first expr)))
+                      (list (first (first expr))
+                            (fifth (first expr))))
+                     (t
+                      (destructuring-bind
+                          (first &optional second third &rest ign) (first expr)
+                        (declare (ignore ign))
+                        (if (symbolp first)
+                            first
+                            (nconc (list (nth 2 first)
+                                         (nth 6 first))
+                                   (unless (null second)
+                                     (list :type (nth 3 second)))
+                                   (unless (null third)
+                                     (list (nth 2 third)
+                                           (nth 6 third)))))))))))
 
 (define-moonli-macro defstruct
   ((name expr:symbol)
-   (_ *whitespace)
+   (_ *whitespace/internal)
+   (_ ":")
+   (_ *whitespace/all)
    (slots (* defstruct/slot)))
   `(defstruct ,name ,@slots))
 
 (def-test defstruct (macro-call)
-  (:moonli "defstruct foo
+  (:moonli "defstruct foo:
   a;
   b;
 end"
    :lisp (defstruct foo a b))
-  (:moonli "defstruct foo
+  (:moonli "defstruct foo:
   (a = 4) :: number;
   b;
 end"
    :lisp (defstruct foo (a 4 :type number) b))
-  (:moonli "defstruct foo
+  (:moonli "defstruct foo:
   (a = 4), :read-only = t;
   b;
 end"
    :lisp (defstruct foo (a 4 :read-only t) b))
-  (:moonli "defstruct foo
+  (:moonli "defstruct foo:
   (a = 4), :read-only = t;
   (b = 2.0) :: single-float, :read-only = t;
 end"
    :lisp (defstruct foo
            (a 4 :read-only t)
-           (b 2.0 :type single-float :read-only t))))
+           (b 2.0 :type single-float :read-only t)))
+  (:moonli "defstruct foo:
+  a = 4;
+  b = 2.0;
+end"
+   :lisp (defstruct foo
+           (a 4)
+           (b 2.0))))
