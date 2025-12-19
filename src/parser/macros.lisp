ff4171fb4194f4d81160ae4b3c0b102b0e83cb6a
Shubhamkar Ayare <digikar@proton.me>
2025-12-19 18:48:34 +0530

    [int] macros are their own parse rules

--- src/parser/macros.lisp
+++ src/parser/macros.lisp
@@ -22,7 +22,7 @@
 
 
 (defmacro define-moonli-macro (name &body (moonli-macro-bindings . body))
-  (alexandria:with-gensyms (expr subexpr args fn idx symbol)
+  (alexandria:with-gensyms (expr subexpr args oidx idx symbol)
     (let* ((namep (namep-symbol name))
            (macro-rule `(and (,namep expr:symbol)
                              +whitespace
@@ -33,55 +33,60 @@
       `(progn
          (defun ,namep (,symbol)
            (eq ,symbol ',name))
-         (destructuring-bind (&optional ,fn &rest ,idx)
-             (gethash ',name *moonli-macro-functions*)
-           (let* ((,expr (esrap:rule-expression
-                          (esrap:find-rule 'macro-call)))
-                  (,subexpr ,expr)
-                  (,idx (or ,idx (1- (length ,subexpr)))))
-             (if ,fn
-                 (setf (nth ,idx (cdr ,subexpr))
-                       ',macro-rule)
-                 (setf (cdr ,subexpr)
-                       (nconc (cdr ,subexpr)
-                              (list ',macro-rule))))
-             (esrap:change-rule 'macro-call ,expr)
-             (setf (gethash ',name *moonli-macro-functions*)
-                   (cons (lambda (,args)
-                           (optima:ematch (rest ,args)
-                             ((list ,@(mapcar #'first moonli-macro-bindings)
-                                    _ "end" _ (or nil ',name))
-                              ,@body)))
-                         ,idx))))))))
+         (esrap:defrule ,name
+             ,macro-rule
+           (:function (lambda (,args)
+                        (declare (optimize debug))
+                        (optima:ematch (cddr ,args)
+                          ((list ,@(mapcar #'first moonli-macro-bindings)
+                                 _ "end" _ (or nil ',name))
+                           ,@body))))
+           (:error-report t))
+         (let* ((,oidx (gethash ',name *moonli-macro-functions*))
+                (,expr (esrap:rule-expression
+                        (esrap:find-rule 'macro-call)))
+                (,subexpr ,expr)
+                (,idx (or ,oidx (1- (length ,subexpr)))))
+           (if ,oidx
+               (setf (nth ,oidx (cdr ,subexpr))
+                     ',name)
+               (setf (cdr ,subexpr)
+                     (nconc (cdr ,subexpr)
+                            (list ',name))))
+           (esrap:change-rule 'macro-call ,expr)
+           (setf (gethash ',name *moonli-macro-functions*)
+                 ,idx))))))
 
 (defmacro define-moonli-short-macro
     (name &body (moonli-macro-bindings . body))
-  (alexandria:with-gensyms (expr subexpr args fn idx symbol)
+  (alexandria:with-gensyms (expr subexpr args oidx idx symbol)
     (let* ((namep (namep-symbol name))
            (macro-rule `(and (,namep expr:symbol)
                              +whitespace/internal
                              ,@(mapcar #'second moonli-macro-bindings))))
       `(progn
          (defun ,namep (,symbol) (eq ,symbol ',name))
-         (destructuring-bind (&optional ,fn &rest ,idx)
-             (gethash ',name *moonli-short-macro-functions*)
-           (let* ((,expr (esrap:rule-expression
-                          (esrap:find-rule 'short-macro-call)))
-                  (,subexpr ,expr)
-                  (,idx (or ,idx (1- (length ,subexpr)))))
-             (if ,fn
-                 (setf (nth ,idx (cdr ,subexpr))
-                       ',macro-rule)
-                 (setf (cdr ,subexpr)
-                       (nconc (cdr ,subexpr)
-                              (list ',macro-rule))))
-             (esrap:change-rule 'short-macro-call ,expr)
-             (setf (gethash ',name *moonli-short-macro-functions*)
-                   (cons (lambda (,args)
-                           (optima:ematch (rest ,args)
-                             ((list ,@(mapcar #'first moonli-macro-bindings))
-                              ,@body)))
-                         ,idx))))))))
+         (esrap:defrule ,name
+             ,macro-rule
+           (:function (lambda (,args)
+                        (optima:ematch (cddr ,args)
+                          ((list ,@(mapcar #'first moonli-macro-bindings))
+                           ,@body))))
+           (:error-report t))
+         (let* ((,oidx (gethash ',name *moonli-short-macro-functions*))
+                (,expr (esrap:rule-expression
+                        (esrap:find-rule 'short-macro-call)))
+                (,subexpr ,expr)
+                (,idx (or ,oidx (1- (length ,subexpr)))))
+           (if ,oidx
+               (setf (nth ,oidx (cdr ,subexpr))
+                     ',name)
+               (setf (cdr ,subexpr)
+                     (nconc (cdr ,subexpr)
+                            (list ',name))))
+           (esrap:change-rule 'short-macro-call ,expr)
+           (setf (gethash ',name *moonli-short-macro-functions*)
+                 ,idx))))))
 
 ;; (setf *moonli-macro-functions* (make-hash-table))
 (when (zerop (hash-table-count *moonli-macro-functions*))
@@ -90,23 +95,10 @@
                    'esrap:rule
                    :expression
                    (copy-tree `(or) ;; This will be filled by the macro
-                                    )
-                   :transform
-                   (lambda (production start end)
-                     (declare (ignorable start end))
-                     ((lambda (expr)
-                        ;; (print expr)
-                        (expand-moonli-macro expr))
-                      production))))
+                                    )))
   (esrap:add-rule 'short-macro-call
                   (make-instance
                    'esrap:rule
                    :expression (copy-tree
-                                `(or))
-                   :transform
-                   (lambda (production start end)
-                     (declare (ignorable start end))
-                     ((lambda (expr)
-                        (expand-moonli-short-macro expr))
-                      production)))))
+                                `(or)))))
 
