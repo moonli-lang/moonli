0bbdf9e73cbd31110a618044e3feaa706355c9a9
digikar <digikar@proton.me>
2026-04-29 22:02:06 +0200

    [add] for basic binary: free args are treated as script files

--- src/binary.lisp
+++ src/binary.lisp
@@ -60,28 +60,29 @@
   (moonli:transpile-moonli-file arg))
 
 (defun main (&optional (argv nil argvp))
-  (multiple-value-bind (options free-args)
-      (handler-case
-          (if argvp (opts:get-opts argv) (opts:get-opts))
-        (error (e)
-          (format uiop:*stderr* "~a: ~a"
-                  (class-name (class-of e))
-                  e)
-          (uiop:print-backtrace :stream uiop:*stderr* :condition e)
-          (format t "try `moonli --help`~&")
-          (uiop:quit 1)))
-    (alexandria:doplist (key arg options)
-      (process-option key arg))
-    (handler-bind ((error
-                     (lambda (c)
-                       (format *error-output* "~A" c)
-                       (uiop:print-backtrace
-                        :condition c :stream *error-output*)
-                       (uiop:quit 1))))
-      (dolist (file-name free-args)
-        (process-option :load file-name)
-        (uiop:quit 0))))
-  (let ((*package* (find-package :moonli-user)))
+  (let ((*package* (find-package :moonli-user))
+        (*print-case* :downcase))
+    (multiple-value-bind (options free-args)
+        (handler-case
+            (if argvp (opts:get-opts argv) (opts:get-opts))
+          (error (e)
+            (format uiop:*stderr* "~a: ~a"
+                    (class-name (class-of e))
+                    e)
+            (uiop:print-backtrace :stream uiop:*stderr* :condition e)
+            (format t "try `moonli --help`~&")
+            (uiop:quit 1)))
+      (alexandria:doplist (key arg options)
+        (process-option key arg))
+      (handler-bind ((error
+                       (lambda (c)
+                         (format *error-output* "~A" c)
+                         (uiop:print-backtrace
+                          :condition c :stream *error-output*)
+                         (uiop:quit 1))))
+        (dolist (file-name free-args)
+          (process-option :load file-name)
+          (uiop:quit 0))))
     (loop :initially (write-string "* ")
                      (force-output)
           :for result := (eval (read-moonli-from-stream *standard-input* nil))
